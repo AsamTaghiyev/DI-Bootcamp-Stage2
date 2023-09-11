0b183431b3316e98245a49cc23dc834638f299b4
AsamTaghiyev <asem.muradov@gmail.com>
2023-09-11 18:29:03 +0400

    day1

---
+++ Desktop/Week2/Day1/dialychalenge/script.js
@@ -0,0 +1,13 @@
+
+
+fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift("Banana");
+fruits.sort();
+fruits.push("kivi");
+fruits.splice(0,1)
+fruits.reverse();
+console.log(fruits);
+
+
+
---
+++ Desktop/Week2/Day1/exercises/script.js
@@ -0,0 +1,183 @@
+
+//Exercise 1 : Series//
+
+let favoriteFood = "pitza";
+let favoriteMeal = "lunch or dinner";
+
+console.log("I eat " + favoriteFood + " at every " + favoriteMeal);
+
+
+
+//Exercise 2 : Series//
+
+
+myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+myWatchedSeries.splice(2,1,"friends");
+
+myWatchedSeries.push("stringer");
+
+myWatchedSeries.unshift("breaking bad");
+myWatchedSeries.splice(1,1);
+
+console.log(myWatchedSeries);
+
+
+
+
+//Exercise 3 : Series//
+
+
+let celsiusTemperature = 25; 
+let fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+
+console.log(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F.");
+
+
+
+
+//Exercise 4 : Series//
+
+let c;
+    let a = 34;
+    let b = 21;
+
+    console.log(a+b) //first expression
+    // Prediction:55
+    // Actual:55
+
+    a = 2;
+
+    console.log(a+b) //second expression
+    // Prediction:23
+    // Actual:23
+
+
+
+    //Exercise 4 : Series//
+
+
+typeof(15)
+// Prediction:number
+// Actual:number
+
+typeof(5.5)
+// Prediction:number
+// Actual:number
+
+typeof(NaN)
+// Prediction:number
+// Actual:number
+
+typeof("hello")
+// Prediction:string
+// Actual:string
+
+typeof(true)
+// Prediction:bolean
+// Actual:bolean
+
+typeof(1 != 2)
+// Prediction:
+// Actual:
+
+"hamburger" + "s"
+// Prediction:hamburgers
+// Actual:hamburgers
+
+"hamburgers" - "s"
+// Prediction:nan
+// Actual:nan
+
+"1" + "3"
+// Prediction:13
+// Actual:13
+
+"1" - "3"
+// Prediction:-2
+// Actual:-2
+
+"johnny" + 5
+// Prediction:johnny5
+// Actual:johnny5
+
+"johnny" - 5
+// Prediction:nan
+// Actual:nan
+
+99 * "hello"
+// Prediction:nan
+// Actual:nan
+
+1 != 1
+// Prediction:false
+// Actual:false
+
+1 == "1"
+// Prediction:true
+// Actual:true
+
+1 === "1"
+// Prediction:false
+// Actual:false
+
+
+
+//Exercise 6 : Series//
+
+
+5 + "34"
+// Prediction: 534
+// Actual:534
+
+5 - "4"
+// Prediction:1
+// Actual:1
+
+10 % 5
+// Prediction:1
+// Actual:11
+
+5 % 10
+// Prediction:5
+// Actual:5
+
+"Java" + "Script"
+// Prediction:JavaScript
+// Actual:JavaScript
+
+" " + " "
+// Prediction:
+// Actual:
+
+" " + 0
+// Prediction:0
+// Actual:0
+
+true + true
+// Prediction:2
+// Actual:2
+
+true + false
+// Prediction:1
+// Actual:1
+
+false + true
+// Prediction:1
+// Actual:1
+
+false - true
+// Prediction:
+// Actual:-1
+
+!true
+// Prediction:false
+// Actual:false
+
+3 - 4
+// Prediction:-1
+// Actual:-1
+
+"Bob" - "bill"
+// Prediction:nan
+// Actual:nan
+
