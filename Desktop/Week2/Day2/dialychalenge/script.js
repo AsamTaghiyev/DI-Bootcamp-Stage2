7c909e81a5a6a0a0e7b318b73b3a652bb3a43978
AsamTaghiyev <asem.muradov@gmail.com>
2023-09-12 14:53:15 +0400

    day2

---
+++ Desktop/Week2/Day2/dialychalenge/script.js
@@ -0,0 +1,32 @@
+// let sentence = "The movie is not that bad, I liked it";
+
+// let wordNot = sentence.indexOf("not");
+
+// let wordBad = sentence.indexOf("bad");
+
+// if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+//     let first = sentence.substring(0, wordNot);
+//     let second = "good";
+//     let third = sentence.substring(wordBad + 3); 
+//     let newSentence = first + second + third;
+//     console.log(newSentence);
+// } else {
+//     console.log(sentence);
+// }
+
+
+// Kullanıcıdan notu al
+// var not = prompt("Lütfen notunuzu girin:");
+
+// // Notun değerine göre duruma karar ver
+// if (not > 90) {
+//     console.log("A");
+// } else if (not >= 80) {
+//     console.log("B");
+// } else if (not >= 70) {
+//     console.log("C");
+// } else {
+//     console.log("D");
+// }
+
+
---
+++ Desktop/Week2/Day2/ecercises/script.js
@@ -0,0 +1,63 @@
+// EXERCISES 1
+
+
+// let x = 5;
+// let y = 2;
+
+// if (x > y) {
+//     console.log("x is the biggest number");
+// } else if (y > x) {
+//     console.log("y is the biggest number");
+// } else {
+//     console.log("x and y are equal");
+// }
+
+
+// EXERCISES 2
+
+
+// let newDog = "Chihuahua";
+
+// console.log("newDog variable contains " + newDog.length + " letters.");
+
+// console.log(newDog.toUpperCase());
+// console.log(newDog.toLowerCase());
+
+// if (newDog === "Chihuahua") {
+//     console.log("Chihuahuas are my favorite dog breed. I love them!");
+// } else {
+//     console.log("I don't care, I prefer cats.");
+// }
+
+
+
+// EXERCISES 3
+
+
+// let userNumber = prompt("Please enter a number:");
+
+// let number = parseInt(userNumber);
+
+// if (number % 2 === 0) {
+//     console.log(number + " is an even number.");
+// } else {
+//     console.log(number + " is a single number.");
+// }
+
+
+
+// EXERCISES 4
+
+
+// let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+
+// if (users.length === 0) {
+//     console.log("no one is online");
+// } else if (users.length === 1) {
+//     console.log(users[0] + " is online");
+// } else if (users.length === 2) {
+//     console.log(users[0] + " and " + users[1] + " are online");
+// } else {
+//     const moreUsersCount = users.length - 2;
+//     console.log(users[0] + ", " + users[1] + " and " + moreUsersCount + " more are online");
+// }
